d2ea4b484ad75542efc5d6a25fa8619327a53e18
Lsans80 <luisa.sans@gmail.com>
2023-12-06 13:57:07 +0100

    S04T01N01

---
+++ Sprint-4/S04T01N01SansLuisa/S04T01N01SansLuisa/src/main/java/cat/itacademy/barcelonactiva/sans/luisa/s04/t01/n01/Controllers/HelloWorldController.java
@@ -0,0 +1,33 @@
+package cat.itacademy.barcelonactiva.sans.luisa.s04.t01.n01.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    private String nombre;
+
+
+    //http://localhost:9000/HelloWorld
+    @GetMapping("/HelloWorld")
+    public String saluda (@RequestParam(defaultValue = "UNKNOWN") String nombre){
+
+        return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven";
+    }
+
+
+    //http://localhost:9000/HelloWorld2
+    //http://localhost:9000/HelloWorld2/Luisa
+    @GetMapping(value = {"/HelloWorld2/{nombre}", "/HelloWorld2"})
+    public String saluda2 (@PathVariable(required = false) String nombre){
+
+        if (nombre == null){
+            nombre = "Unknown";
+        }
+
+        return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven";
+    }
+}
