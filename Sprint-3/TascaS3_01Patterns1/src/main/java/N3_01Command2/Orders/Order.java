b925da51a2b90783839d274ed7a6ab5214c16447
Luisa Sans <122398607+Lsans80@users.noreply.github.com>
2024-01-05 17:50:08 +0100

    Delete Sprint-3/TascaS3_01Patterns1/src/main/java/N3_01Command2 directory

--- Sprint-3/TascaS3_01Patterns1/src/main/java/N3_01Command2/Orders/Order.java
+++
@@ -1,6 +0,0 @@
-package N3_01Command2.Orders;
-
-public interface Order {
-
-    void execute();
-}
--- Sprint-3/TascaS3_01Patterns1/src/main/java/N3_01Command2/Parking/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package N3_01Command2.Parking;
-//Command interface
-public interface Vehicle {
-
-    void startUp();
-    void accelerate();
-    void slowDown();
-}
