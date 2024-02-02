7f293e103814665c5ad8e7a37fd0349b7fa3f93c
Lsans80 <luisa.sans@gmail.com>
2024-02-02 12:40:55 +0100

    S4T1N1

---
+++ Sprint-4/Tasca 1/S04T01N01SansLuisa/S04T01N01SansLuisa/src/test/java/cat/itacademy/barcelonactiva/sans/luisa/s04/t01/n01/S04T01N01SansLuisaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.sans.luisa.s04.t01.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01SansLuisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
