a9ed459f7be57cef83a3fd9edd57c69ded417b68
Luisa Sans <122398607+Lsans80@users.noreply.github.com>
2024-02-13 21:43:48 +0100

    Delete Sprint-5/Tasca 2/S05T02N01F3TestSansMollLuisa directory

--- Sprint-5/Tasca 2/S05T02N01F3TestSansMollLuisa/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t02/n01/f3/S05T02N01F3SansMollLuisa/exceptions/UserAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.cognoms.nom.s05.t02.n01.f3.S05T02N01F3SansMollLuisa.exceptions;
-
-public class UserAlreadyExists extends RuntimeException{
-    public UserAlreadyExists(String message) {
-        super(message);
-    }
-}
